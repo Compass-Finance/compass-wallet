99cffbe1baa746400582ba8d127e8ca2755897c7
Sonny Vesali <sonny.vesali@proton.me>
2022-09-13 15:47:31 -0500

    finally builds on android

--- app.config.js
+++ app.config.js
@@ -35,7 +35,7 @@ export default {
     android: {
       // runtimeVersion: '1.0.0',
       adaptiveIcon: {
-        foregroundImage: './assets/icon.png',
+        foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#FFFFFF',
       },
       package: 'xyz.compass.fi.compass.wallet.v1',
