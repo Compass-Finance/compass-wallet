2fd328757a96759a03ebd22d913fdd4e3e484e0a
Sonny Vesali <sonny.vesali@proton.me>
2022-10-13 17:34:29 -0500

    testnet additions

--- src/logic/services/UserTokenData.service.ts
+++ src/logic/services/UserTokenData.service.ts
@@ -3,10 +3,14 @@ import { UserTokenDataResArr } from '../models/int_models';
 
 export const UserTokenDataGetter = async (addressToQuery: string) => {
   try {
+    console.log(addressToQuery, '<======= is this null ????');
     const tokenDataRes = await supabase.rpc('fetch_user_token_data', {
       address: addressToQuery,
+      the_network: 'polygon',
     });
-    console.log(`THIS IS THE DATA !!! =======> ${tokenDataRes.data}`);
+    // console.log(
+    //   `THIS IS THE DATA !!! =======> ${JSON.stringify(tokenDataRes.data)}`
+    // );
     // @ts-ignore
     return tokenDataRes.data.data as UserTokenDataResArr;
   } catch (e) {
